bf55e714cdada019323888dc1dd9fae83391345b
ihsanghnii <ihsanghan01@gmail.com>
2025-09-23 10:42:44 +0700

    update tugas 4

--- Materi-Javascript/tugas-04/app.js
+++ Materi-Javascript/tugas-04/app.js
@@ -11,13 +11,49 @@ class Pelanggan {
     sewa(kendaraan) {
         this.#kendaraanDisewa = kendaraan;
     }
+
+    getNama() {
+        return this.#nama;
+    }
+
+    getTelpon() {
+        return this.#nomorTelepon;
+    }
+
+    getKendaraan() {
+        return this.#kendaraanDisewa;
+    }
 }
 
-const pelanggan1 = new Pelanggan("budi", "081234567890" , null);
-console.log("Data Pelanggan :", pelanggan1);
-pelanggan1.sewa("alphard");
-console.log("Data Penyewa :", pelanggan1);
+function tampilkanPelanggan() {
+    console.log(" --- Daftar Pelanggan Saat Ini --- ");
+    for(const pelanggan of daftarPelanggan) {
+        if(pelanggan.getKendaraan() !== null) {
+            console.log(
+                `Nama: ${pelanggan.getNama()}, Telepon: ${pelanggan.getTelpon()}, Kendaraan Sewa: ${pelanggan.getKendaraan()}`
+            );
+        }
+    }
+}
 
-const daftarPelanggan = [
-    
-]
+const daftarPelanggan = [];
+const pelanggan1 = new Pelanggan("budi", "081234567890" , null);
+const pelanggan2 = new Pelanggan("dika", "081232337890" , null);
+const pelanggan3 = new Pelanggan("jaki", "081234567430" , null);
+
+daftarPelanggan.push(pelanggan1);
+daftarPelanggan.push(pelanggan2);
+daftarPelanggan.push(pelanggan3);
+tampilkanPelanggan();
+
+console.log("\n--- Budi menyewa mobil ---");
+pelanggan1.sewa("Mobil");
+tampilkanPelanggan();
+
+console.log("\n--- dika menyewa motor ---");
+pelanggan2.sewa("Motor");
+tampilkanPelanggan();
+
+console.log("\n--- jaki menyewa mobil ---");
+pelanggan3.sewa("Mobil");
+tampilkanPelanggan();
